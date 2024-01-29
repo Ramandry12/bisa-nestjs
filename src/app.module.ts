afe7cd31decd93721634fe76f38ef89f48fe313a
Ramandry12 <andryramdhan58@gmail.com>
2024-01-29 15:22:53 +0700

    first commit deploy

---
+++ src/app.module.ts
@@ -0,0 +1,29 @@
+import { Module } from '@nestjs/common';
+import { ProductsModule } from './tasks/products.module';
+import { MulterModule } from '@nestjs/platform-express';
+import { multerConfig } from './middleware/multer.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+
+@Module({
+  imports: [
+    ProductsModule,
+    MulterModule.register(multerConfig),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'ep-proud-smoke-a1tb4ww6-pooler.ap-southeast-1.postgres.vercel-storage.com',
+      port: 5432,
+      username: 'default',
+      password: 'tjCpEwU8l4Yi',
+      database: 'verceldb',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      autoLoadEntities: true,
+      synchronize: true,
+      ssl: {
+        rejectUnauthorized: true,
+      },
+    }),
+    UserModule,
+  ],
+})
+export class AppModule {}
